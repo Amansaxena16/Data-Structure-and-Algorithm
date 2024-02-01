444a9805fe1ccfd728c09e838d26935779d890c9
Amansaxena16 <aman16nov.as@gmail.com>
2024-02-02 01:25:09 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b , sum;
+    cout << "Enter the number in a: ";
+    cin >> a;
+    cout<<"\nEnter the value for b: ";
+    cin>>b;
+
+    sum = a + b;
+    // cout << "\nSum of a and b = " << sum; 
+    cout<<"\nSum of "<<a<<" and "<<b<<" = "<<sum;
+    return 0;
+}
