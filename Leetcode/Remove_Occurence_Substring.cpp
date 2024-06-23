2404a0804e57a9ceed05054276e7a6ed1e39c1b8
Amansaxena6 <aman16nov.as@gmail.com>
2024-06-23 18:38:21 +0530

    added new files

--- Leetcode/Remove_Occurence_Substring.cpp
+++ Leetcode/Remove_Occurence_Substring.cpp
@@ -1,73 +1,65 @@
 // Writing a program to remove all the occurence of a substring!!
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-string rightShift(string s, int i, int part_len){
-    for(int j = 0; j < part_len; j++){
-        int k = 0;
-        for(k = i + 1; k < s.length(); k++){
-            s[k-1] = s[k];
-        }
-        s[k - 1] = '\0';
-    }
-    return s;  
+string rightShift(string s, int i, int part_len) {
+    s.erase(i, part_len);
+    return s;
 }
 
-string removeOccurrences(string s, string part){
+string removeOccurrences(string s, string part) {
     bool flag = true;
     int part_len = part.length();
 
-    while(flag == true){
+    while(flag) {
+        flag = false;
         char temp = part[0];
-        for(int i = 0; s[i] != '\0'; i++){
-            if(s[i] == temp){
-                bool found = false;
-                i++;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == temp) {
+                bool found = true;
                 int count = 1;
-                for(int j = 1; part[j] != '\0'; j++){
-                    if(s[i] == part[j]){
+
+                for(int j = 1; j < part.length(); j++) {
+                    if(s[i + j] == part[j]) {
                         count++;
-                        i++;
-                        found = true;
-                    }
-                    else{
-                        i = i - count;
+                    } else {
+                        found = false;
                         break;
                     }
                 }
 
-                if(found == true){
-                    i = i - count;
-                    s = rightShift(s,i,part_len);
-                    cout << "s : " << s << endl;
-                    break;
-                }
-            }
-        }
-
-        int b = 0;
-        for(int i = 0; s[i] != '\0'; i++){
-            if(s[i] == part[b]){
-                b++;
-                if(part[b] == '\0'){
+                if(found) {
+                    s = rightShift(s, i, part_len);
+                    cout << "s :" << s << endl;
                     flag = true;
                     break;
                 }
             }
-            else{
+        }
+
+        // Check if any occurrence of part remains
+        int b = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == part[b]) {
+                b++;
+                if(part[b] == '\0') {
+                    flag = true;
+                    break;
+                }
+            } else {
                 b = 0;
-                flag = false;
             }
         }
     }
+
+    return s;
 }
 
-int main(){
-    
-    string s = "axxxyyyb";
-    string part = "xy";
-
-    removeOccurrences(s,part);
+int main() {
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    cout << "New String is : " << removeOccurrences(s, part) << endl;
     return 0;
 }
