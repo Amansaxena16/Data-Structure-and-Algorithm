2f308df49b494a99bffe2dff95b16668f8b05c33
Amansaxena6 <aman16nov.as@gmail.com>
2024-09-25 01:56:52 +0530

    Added new files

--- Pattern/patt_test_40.cpp
+++ Pattern/patt_test_40.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 int main(){
+    
     int n = 5;
 
     for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n-i; j++){
+        for(int j = 1; j < i; j++){
             cout << " ";
         }
-        for(int j = 1; j <= i; j++){
+        for(int j = i; j <= n; j++){
             cout << "*";
         }
-        for(int j = 1; j < i; j++){
+        for(int j = i; j <= n-1; j++){
             cout << "*";
         }
-        cout << "\n";
+        cout << endl;
+        
     }
-    
     return 0;
 }
