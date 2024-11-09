bd8963e207d7ba3133bb9f8b3956a23a4d41202e
Amansaxena16 <aman16nov.as@gmail.com>
2024-11-09 20:05:31 +0530

    added new file

--- Array/arr_test_35.cpp
+++ Array/arr_test_35.cpp
@@ -1,59 +1,92 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
+// void merge(int arr1[], int n, int arr2[], int m){
+//     int size = n + m;
+//     int arr3[size];
+//     int left = 0;
+//     int right = 0;
+//     int i = 0;
+
+//     while(left < n && right < m){
+//         if(arr1[left] < arr2[right]){
+//             arr3[i] = arr1[left];
+//             left++;
+//             i++;
+//         }
+//         else{
+//             arr3[i] = arr2[right];
+//             right++;
+//             i++; 
+//         }
+//     }
+
+//     // Inserting Left over values of arr1 into arr[3]
+//     while(left < n){
+//         arr3[i] = arr1[left];
+//         left++;
+//         i++;
+//     }
+
+//     // Inserting Left over values of arr2 into arr3
+//     while(right < m){
+//         arr3[i] = arr2[right];
+//         right++;
+//         i++;
+//     }
+
+//     // Inserting the values into arr1 and arr2
+//     for(int i = 0; i < size; i++){
+//         if(i < n){
+//             arr1[i] = arr3[i];
+//         }
+//         else{
+//             arr2[i - n] = arr3[i];
+//         }
+//     }
+
+//     // printing the both the loops
+//     for(int i = 0; i < n; i++){
+//         cout << arr1[i] << " "; 
+//     }
+//     cout << endl;
+//     for(int j = 0; j < m; j++){
+//         cout << arr2[j] << " ";
+//     }
+//     cout << endl;
+// }
+
 void merge(int arr1[], int n, int arr2[], int m){
-    int size = n + m;
-    int arr3[size];
-    int left = 0;
-    int right = 0;
-    int i = 0;
+    int last = n - 1;
+    int first = 0;
 
-    while(left < n && right < m){
-        if(arr1[left] < arr2[right]){
-            arr3[i] = arr1[left];
-            left++;
-            i++;
+    while(last >= 0 && first < m){
+        if(arr1[last] > arr2[first]){
+            swap(arr1[last],arr2[first]);
+            last--;
+            first++;
         }
         else{
-            arr3[i] = arr2[right];
-            right++;
-            i++; 
+            break;
         }
     }
 
-    // Inserting Left over values of arr1 into arr[3]
-    while(left < n){
-        arr3[i] = arr1[left];
-        left++;
-        i++;
-    }
+    // sorting both the arrays
+    sort(arr1,arr1 + n);
+    sort(arr2,arr2 + m);
 
-    // Inserting Left over values of arr2 into arr3
-    while(right < m){
-        arr3[i] = arr2[right];
-        right++;
-        i++;
-    }
-
-    // Inserting the values into arr1 and arr2
-    for(int i = 0; i < size; i++){
-        if(i < n){
-            arr1[i] = arr3[i];
-        }
-        else{
-            arr2[i - n] = arr3[i];
-        }
-    }
 
     // printing the both the loops
     for(int i = 0; i < n; i++){
         cout << arr1[i] << " "; 
     }
-    cout << endl;
+    cout << endl; 
     for(int j = 0; j < m; j++){
         cout << arr2[j] << " ";
     }
     cout << endl;
+
 }
 
 int main(){
