c5b0608ae955161d0bf271c60df04083069e6797
Amansaxena16 <aman16nov.as@gmail.com>
2024-11-15 01:44:16 +0530

    added new file

--- Array/arr_test_38.cpp
+++ Array/arr_test_38.cpp
@@ -3,17 +3,28 @@
 using namespace std;
 
 void findNumber(int arr[], int n){
-    int test[n] = {0};
+    int index = n + 1;
+    int test[index] = {0};
     
     for(int i = 0; i < n; i++){
         test[arr[i]]++;
-         
+        if(test[arr[i]] == 2){
+            cout << "Repeating Number is : " << arr[i] << endl;
+        }     
     }
+
+    for(int i = n; i > 0; i--){
+        if(test[i] == 0){
+            cout << "Missing Number is : " << i << endl;
+        }
+    }
+    cout << endl;
 }
+
 
 int main(){
     
-    int arr[6] = {1,3,2,5,4,1};
-    findNumber(arr,6);
+    int arr[4] = {3,3,2,1};
+    findNumber(arr,4);
     return 0;
 }
