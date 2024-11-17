90f58ecf6747d5b5163c7f72de9214725126e259
Amansaxena16 <aman16nov.as@gmail.com>
2024-11-18 00:23:31 +0530

    added new file

--- Array/arr_test_38.cpp
+++ Array/arr_test_38.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 
 void findNumber(int arr[], int n){
-    int index = n + 1;
-    int test[index] = {0};
-    
+    int test[n + 1] = {0};  // create an extra space 
+
+    // Increasing the count of test array of arr[i] value
     for(int i = 0; i < n; i++){
         test[arr[i]]++;
-        if(test[arr[i]] == 2){
+    }
+    
+    // Finding the Repeating and missing number
+    for(int i = 1; i <= n; i++){
+        if(test[i] == 2){
             cout << "Repeating Number is : " << arr[i] << endl;
         }     
-    }
-
-    for(int i = n; i > 0; i--){
-        if(test[i] == 0){
+        else if(test[i] == 0){
             cout << "Missing Number is : " << i << endl;
         }
     }
--- Array/arr_test_39.cpp
+++ Array/arr_test_39.cpp
@@ -2,22 +2,76 @@
 #include<iostream>
 using namespace std;
 
-int getInversion(int arr[], int n){
-    int count = 0;
-    for(int i = 0; i < n -1; i++){
-        for(int j = i + 1; j < n; j++){
-            if(arr[i] > arr[j]){
-                count++;
-            }
-        }
+int count = 0;
+
+void merge(int arr[], int start, int end){
+    int mid = (start + end) / 2;
+
+    int len1 = (mid - start) + 1;
+    int len2 = end - mid;
+
+    int *first = new int[len1];
+    int *second = new int[len2];
+
+    int index = start;
+
+    // copying left part into first array
+    for(int i = 0; i < len1; i++){
+        first[i] = arr[index++];
     }
-    return count;
+
+    index = mid + 1;
+    // copying right part into second array
+    for(int i = 0; i < len2; i++){
+        second[i] = arr[index++];
+    }
+
+    int left = 0; 
+
+    // Checking if first array value is greater then count while increase by len2 value
+    while(left < len1){
+
+        if(first[left] > second[0]){
+            count = count + len2;
+            cout <<"count : " <<  count << endl;
+
+        }
+        left++;
+    }
+
+    // free the dynamic memory allocation 
+    delete[] first;
+    delete[] second;    
 }
+
+void mergeSort(int arr[], int start, int end){
+    int mid = (start + end) / 2;
+
+    if(start >= end){
+        return;
+    }
+
+    // Sorting left half
+    mergeSort(arr,start, mid);
+
+    // Sorting right half
+    mergeSort(arr,mid + 1, end);
+
+    // merging the array
+    merge(arr,start,end);
+
+}
+
+int getInversion(int arr[], int n){
+    mergeSort(arr,0,n-1);
+    return count;
+}   
 
 int main(){
     
     int arr[5] = {2,5,1,3,4};
-    cout << "Number of Inversion in an array is : " <<  getInversion(arr,5) << endl;
+    int n = 5;
+    cout << "Number of Inversion in an array is : " <<  getInversion(arr, n) << endl;
 
     return 0;
 }
