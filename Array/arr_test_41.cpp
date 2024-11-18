75e4b041a1ee2ea17db5b90dbc92f04a29393552
Amansaxena16 <aman16nov.as@gmail.com>
2024-11-19 02:57:31 +0530

    added new file

---
+++ Array/arr_test_41.cpp
@@ -0,0 +1,14 @@
+// writing a program to find the duplicates in an array!!
+#include<iostream>
+using namespace std;
+
+bool findDuplicate(int arr[], int n){
+    
+}
+
+int main(){
+    
+    int arr[4] = {1,2,3,4};
+    cout << findDuplicate(arr,4) << endl;
+    return 0;
+}
