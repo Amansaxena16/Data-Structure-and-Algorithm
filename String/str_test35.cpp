e13a4f52222c8da2d8a2dae0c69c17b04a1d5398
Amansaxena16 <aman16nov.as@gmail.com>
2025-01-23 23:51:04 +0530

    added new file and changes

--- String/str_test35.cpp
+++ String/str_test35.cpp
@@ -2,23 +2,44 @@
 #include<iostream>
 using namespace std;
 
+bool isFreqSame(int freq1[], int freq2[]){
+    for(int i = 0; i < 26; i++){
+        if(freq1[i] != freq2[i]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 bool checkInclusion(string s1, string s2){ 
     int frequency[26] = {0};
-    int windowFreq[26] = {0};
 
     // adding the count of each letter in frequency array
     for(int i = 0; i < s1.length(); i++){
         frequency[s1[i] - 'a']++;
     }
 
-    // 
-    return true;
+    int windowSize = s1.length();
+    for(int i = 0; i < s2.length(); i++){
+        int windowFreq[26] = {0};
+
+        for(int j = i; j < i + windowSize && j < s2.length(); j++){
+            windowFreq[s2[j] - 'a']++;
+        }
+
+        if(isFreqSame(frequency,windowFreq)){
+            return true;
+        }
+    }
+
+    return false;
 }
 
 int main(){
     
     string s1 = "ab";
-    string s2 = "eidbaooo";
+    string s2 = "a";
     cout << checkInclusion(s1,s2) << endl;
     return 0;
 }
