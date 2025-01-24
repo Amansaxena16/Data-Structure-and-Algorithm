c43d37b1f499a5d84b8bfae0ee8f4e4e57f5844a
Amansaxena16 <aman16nov.as@gmail.com>
2025-01-25 00:55:15 +0530

    added new files and changes

--- String/str_test38.cpp
+++ String/str_test38.cpp
@@ -2,13 +2,45 @@
 #include<iostream>
 using namespace std;
 
-void longestCommonPrefix(string str){
-    
+void longestCommonPrefix(string str[], int n){
+    bool flag = true;
+    string prefixChars = "";
+    cout << n << endl;
+    if(n == 1){
+        cout << str[0] << endl;
+        return;
+    }
+
+    for(int i = 0; i < str[0].size(); i++){
+        for(int j = 1; j < n; j++){
+
+            if(i >= str[j].size() || str[0][i] != str[j][i]){
+                flag = false;
+                break;
+            } 
+        }
+
+        if(flag){
+            prefixChars = prefixChars + str[0][i];
+        }
+        else{
+            break;
+        } 
+    }
+
+    if(prefixChars.size() == 0){
+        cout << "No Prefix Found!!" << endl;
+    }
+    else{
+        cout << "Prefix Characters are : " << prefixChars << endl;
+    }
 }
 
 int main(){
     
-    string str = {"flower","flow","flight"};
-    longestCommonPrefix(str);
+    string str[] = {"flower","flow","flight"};
+    int n = sizeof(str) / sizeof(str[0]);
+
+    longestCommonPrefix(str,n);
     return 0;
 } 
