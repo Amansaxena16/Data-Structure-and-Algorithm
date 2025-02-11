ec86d1c7d6afc7a21677316df3e8320f6bb24fb9
Amansaxena16 <aman16nov.as@gmail.com>
2025-02-11 23:40:22 +0530

    added new file

---
+++ Linked List/link_test6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    int *ptr;
+
+    Node(int data){
+        this -> data = data;
+        ptr = NULL;
+    }
+};
+
+class List{
+    
+};
+
+int main(){
+    
+    
+    return 0;
+}
