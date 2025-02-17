40a5c5b6f5c8bae694cd05bd3be2c614b9bc87eb
Amansaxena16 <aman16nov.as@gmail.com>
2025-02-17 19:05:02 +0530

    added new file

--- Linked List/link_test11.cpp
+++ Linked List/link_test11.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 class Node{
+public:
     int data;
     Node *next;
     Node *random;
@@ -15,21 +16,48 @@ class Node{
 };  
 
 class List1{
+private:
     Node *head;
     Node *tail;
 
+public:
     List1(){
         head = NULL;
         tail = NULL;
     }
 
-    void push_back(int data){
-        
+    void push_back(int data){   
+        Node *newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        else{
+            tail -> next = newNode;
+            tail = newNode; 
+        }
+    }
+
+    void printList(){
+        Node *temp = head;
+        while(temp != NULL){
+            cout << temp -> data << "->";
+            temp = temp -> next;
+        }
+        cout << "NNULL" << endl;
     }
 };
 
 int main(){
     
-    
+    List1 l1;
+    l1.push_back(7);
+    l1.push_back(13);
+    l1.push_back(11);
+    l1.push_back(10);
+    l1.push_back(1);
+
+    l1.printList();
     return 0;
 }
